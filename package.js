72b6b82610c75160bd0ea620146ca354d489ffb7
Mike Cunneen <mike@appworkshop.net>
2025-03-11 11:06:47 +0800

    feat: :wrench: package.js should refer to package.json for metadata
    - Using a tool such as release-it is much simpler
    if we use package.json as the "single source of
    truth" for version numbers etc.
    - NOTE: This uses a non-public Meteor API
    (Package._packageSource)

--- package.js
+++ package.js
@@ -1,28 +1,34 @@
+const fs = Npm.require("fs");
+const path = Npm.require("path");
+
+// try to obtain package metadata from package.json ; it's much easier to manage the version number this way
+// TODO: try to find a better way to obtain package metadata from package.json ; currently relying on a non-public _packageSource
+const packageJSON = JSON.parse(fs.readFileSync(path.join(Package._packageSource.sourceRoot, "package.json")));
+
+const { description, version, repository } = packageJSON;
 Package.describe({
   name: "cunneen:tooltips",
-  summary: "Reactive tooltips, no coffee required.",
-  version: "1.0.0",
-  git: "https://github.com/cunneen/meteor-tooltips.git",
+  summary: description || "Reactive tooltips, no coffee required.",
+  version: version || "1.0.1",
+  git: repository || "https://github.com/cunneen/meteor-tooltips.git",
 });
 
 Package.onUse(function (api) {
   api.use(
-    ["ecmascript@0.0.0", 
-      "reactive-var@1.0.0", 
-      "jquery@1.0.0 || 3.0.0", 
-      "templating@1.0.0", 
-      "tracker@1.0.0"],
+    [
+      "ecmascript@0.0.0",
+      "reactive-var@1.0.0",
+      "jquery@1.0.0 || 3.0.0",
+      "templating@1.0.0",
+      "tracker@1.0.0",
+    ],
     "client"
   );
   const distDir = "dist";
+
   api.addFiles(
-    [
-      "package-scoped-var-declarations.js",
-      "tooltips.html",
-      "lodashUtils.js",
-      "tooltips.js",
-    ].map((f) => `${distDir}/${f}`),
+    ["tooltips.html", "lodashUtils.js"].map((f) => `${distDir}/${f}`),
     "client"
   );
-  api.export("Tooltips", "client");
+  api.mainModule(`${distDir}/tooltips.js`, "client");
 });
